06b024d573b7b0bc7ab857b54b1dda8333862a3d
ikeahat <wolff.luna@protonmail.com>
2025-01-26 23:30:42 +0100

    add_to_transaction_history()

--- vereinskasse.py
+++ vereinskasse.py
@@ -1,15 +1,15 @@
-__author__ = '8568922, Wolff'
+# TODO author
 
-import Tkinker
-
+from Tkinker import tk
+from datetime import datetime
 def account_perm_string(number):
     return ["Treasurer", "Finance Officer", "Admin"][number]
 
 class Account:
-    def __init__(self, name, password, account_type: int, department=None):
+    def __init__(self, name, password, type: int, department=None):
         self.name = name
         self.password = password
-        self.account_type = account_type
+        self.type = type
         self.department = department
     
     def __str__(self):
@@ -25,7 +25,6 @@ def display_departments():
     for idx, department in enumerate(departments):
         print(f"{idx}: {department['name']}")
 
-
 class Department:
     def __init__(self, name, balance: int):
         self.name = name
@@ -33,15 +32,59 @@ class Department:
     def can_pay(self, balance, amount) -> bool:
         return self.balance - self.amount >= 0
     def withdrawal(self, amount: int):
+        type = "withdrawal"
+        text = "has withdrawn"
         amount = input("amount:")
         if self.can_pay() == True:
             self.balance = self.balance - amount
+        a = Transaction(self, amount, type, text)
+        self.transactions.append(a)
     def deposit(self, amount: int):
+        type = "deposit"
+        text = "has deposited"
         amount = input("amount:")
         self.balance = self.balance + amount
+        a = Transaction(self, amount, type, text)
+        self.transactions.append(a)
     def transfer(self, amount: int, recipient: Department):
         display_departments()  # Choose recipient
+        type = "transfer"
+        text = "has transfered"
         amount = input("amount:")
         if self.can_pay() == True:
             self.balance = self.balance - amount
             recipient.balance = recipient.balance + amount
+        a = Transaction(self, amount, type, text)
+        self.transactions.append(a)
+
+transactions = []
+
+class Transaction:
+    def __init__(self, amount, type, text, sender_name):
+        self.sender_name = sender_name
+        self.type = type
+        self.amount = amount
+        self.text = text
+        self.time = str(datetime.now())
+
+class System:
+    def __init__(self, title, accounts: list, departments: list):
+        self.title = title
+        self.accounts = []
+        self.departments = []
+    def create_account(self, name, password, type: int, department = None):
+        a = Account(name, password, type, department)
+        self.accounts.append(a)
+    def find_account(self, name):
+        for acc in self.accounts:
+            if acc.name == name:
+                return acc
+    def add_to_transaction_history():
+        transaction_filename = f"transaction history: {Department}.txt"
+        with open(transaction_filename, "a") as f:
+            for transaction in transactions:
+                f.write(transaction.time)
+                if transaction.type == "withdrawal" or "deposit":
+                    f.write({transaction.sender_name} + {transaction.text} + {transaction.amount})
+                elif transaction.type == "transfer":
+                    f.write(f"{transaction.sender_name} + {transaction.text} + {transaction.amount} to {transaction.recipient}")
