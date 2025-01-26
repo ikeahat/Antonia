9a41fd6366d1ca74cbb24d46460cecfb2952fc69
TheGreatSoldeir <silence.marko@gmail.com>
2025-01-26 19:43:42 +0100

    First steps...

--- vereinskasse.py
+++ vereinskasse.py
@@ -2,6 +2,19 @@ __author__ = '8568922, Wolff'
 
 import Tkinker
 
-class Mitglied
-param: 
-    name, age, abteilung, UID, 
+def account_perm_string(number):
+    return ["Treasurer", "Finance Officer", "Admin"][number]
+
+
+class Account:
+    def __init__(self, name, password, account_type, department=None):
+        self.name = name
+        self.password = password
+        self.account_type = account_type
+        self.department = department
+    
+    def __str__(self):
+        s = f"First name: {self.name[0]}\nLast name: {self.name[1]}\nAccount type: {account_perm_string(self.account_type)}"
+        if self.department is not None:
+            s += f"\nDepartment: {str(self.department)}"
+        return s
