6bf213a8c0f2eb8bdbb9f1c2fe6b9e3f3321f6cb
TheGreatSoldeir <silence.marko@gmail.com>
2025-01-27 16:51:09 +0100

    gui stuff 1

--- GUI.py
+++ GUI.py
@@ -1,2 +1,24 @@
 import tkinter as tk
+from vereinskasse import *
 
+class SystemGUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.sys = System("title", [], [])
+    def clear(self):
+        """Clear all widgets from tk screen"""
+        for widget in self.root.winfo_children():
+            widget.destroy()
+    def start(self):
+        self.root.mainloop()
+    def login_gui(self):
+        selected = tk.StringVar()
+        login_names = [acc.name for acc in self.sys.accounts]
+        om = tk.OptionMenu(self.root, login_names, *login_names)
+        om.pack()
+
+
+gui = SystemGUI()
+gui.sys.create_account(("admin", "admin"), "hiabc", 0)
+gui.start()
+gui.login_gui()
