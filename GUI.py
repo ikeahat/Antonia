bd1a66365040f3e7077220b6f7be900ba15db4b6
ikeahat <wolff.luna@protonmail.com>
2025-01-29 16:24:22 +0100

    Revert "random shit??"
    This reverts commit 52e67164cf2e818e2937445ae00cbd823361e591.

--- GUI.py
+++ GUI.py
@@ -7,39 +7,38 @@ class SystemGUI:
     def __init__(self):
         self.root = tk.Tk()
         self.sys = System("title", [], [])
+
+        admin_account = Account(("f", "l"), "abc", "admin")
+        self.sys = System("title", [admin_account], [])
     def clear(self):
         """Clear all widgets from tk screen"""
         for widget in self.root.winfo_children():
             widget.destroy()
     def start(self):
+        self.login_gui()
         self.root.mainloop()
     def login_gui(self):
         selected = tk.StringVar()
         login_names = [acc.name for acc in self.sys.accounts]
-
         username_menu = tk.OptionMenu(self.root, selected, *login_names)
         username_menu.grid(row=0,column=1)
-
-        tk.Label(self.root, "Nutzer").grid(row=0,column=0)
-        tk.Label(self.root, "Passwort").grid(row=1, column=0)
-        
-
-        password_entry = tk.Entry(self.root)
+        tk.Label(self.root, text="Nutzer").grid(row=0,column=0)
+        tk.Label(self.root, text="Passwort").grid(row=1, column=0)
+        password_entry = tk.Entry(self.root, show="*", bg="black", fg="white")
         password_entry.grid(row=1,column=1)
 
-
-
-root = tk.Tk()
-
-b0 = ttk.Button(root, text = "transfer", command = Department.transfer)
+'''b0 = ttk.Button(root, text = "transfer", command = Department.transfer)
 b0.pack()
 b1 = ttk.Button(root, text = "Doch", command = hallo)
 b1.pack()
 b2 = tk.Button(root, text="Nein", command = root.destroy)
-b2.pack()
-root.mainloop()
+b2.pack()'''
 
 '''gui = SystemGUI()
 gui.sys.create_account(("admin", "admin"), "hiabc", 0)
 gui.login_gui()
 gui.start()'''
+
+if __name__ == "__main__":
+    gui = SystemGUI()
+    gui.start()
